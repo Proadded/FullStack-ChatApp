c08a59f928254253e8c780826930873bff1d06c4
Aditya Verma <meaditya14.verma@gmail.com>
2025-06-26 01:22:25 +0530

    Fixed socket.js

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,24 +8,23 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
-  },  
+  },
 });
 
-export function getReceiverSocketId(userId){
+export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// Used to store online user
-const userSocketMap = {}; // {userId: soketId}
-
+// used to store online users
+const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if(userId) userSocketMap[userId] = socket.id
+  if (userId) userSocketMap[userId] = socket.id;
 
-  // used to send events to all the connected clients
+  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
