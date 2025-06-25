0abb2b8770e3c80eb8f73937b8ecda97f1da2f9f
Aditya Verma <meaditya14.verma@gmail.com>
2025-06-26 00:52:59 +0530

    Fix __dirname issue for deployment on Render

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,6 @@ import path from "path";
 dotenv.config()
 
 const PORT = process.env.PORT
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser()); 
@@ -24,13 +23,16 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+const __dirname = path.resolve();
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist", "index.html"));
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
+
 
 server.listen(PORT, () => {
     console.log("server is running on PORT:" + PORT);
